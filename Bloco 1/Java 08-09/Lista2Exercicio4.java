f9903012e387cf5cffdaeadf668ed077e848ee24
isabel-brolhato <isabel.brolhato@gmail.com>
2022-09-08 16:09:34 -0300

    Adds 2 new exercises

---
+++ Bloco 1/Java 08-09/Lista2Exercicio4.java
@@ -0,0 +1,31 @@
+//1-	Faça um programa em que permita a entrada de um número qualquer e exiba se este número é par ou ímpar. Se for par exiba também a raiz quadrada do mesmo; se for ímpar exiba o número elevado ao quadrado.
+
+package generation;
+
+import java.util.Scanner;
+
+public class Lista2ex4 {
+
+	public static void main(String[] args) {
+		
+		int numero;
+		double resultado;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("\nEntre com um número qualquer: ");
+		numero = leia.nextInt();
+		
+		if(numero % 2 == 0) {
+			resultado = Math.sqrt(numero);
+		}else {
+			resultado = Math.pow(numero, 2);
+		}
+		
+		System.out.println("\nResultado: "+resultado);
+
+	}
+
+}
+
+
